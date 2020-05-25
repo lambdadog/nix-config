bd568ee9171fe71c7d25c2af4bfa1099b3e4350c
Ashlynn Anderson <pea@pea.sh>
2020-05-25 09:40:36 -0400

    Resolve zettel--validate issue

--- emacs/zettel.el
+++ emacs/zettel.el
@@ -7,18 +7,42 @@
 
 (use-package deft
   :bind ("C-c d" . deft)
-  :commands (deft-mode
-	     deft-new-file-named)
+  :commands (deft-new-file-named)
   :custom
   (deft-extensions '("org"))
   (deft-directory zettel-dir)
-  (deft-use-filename-as-title t))
+  (deft-use-filename-as-title t)
+  (deft-auto-save-interval 0))
 
-;; We need a minor mode so that we can create ryo-modal bindings for
-;; working with my zettelkasten.
 (define-minor-mode zettelkasten-mode
   "A mode for editing org files in my zettelkasten"
-  :lighter " Zettel")
+  :lighter " Zettel"
+  (cond
+   (zettelkasten-mode
+    (require 'deft)
+    (when (not (get-buffer deft-buffer))
+      (with-current-buffer (get-buffer-create deft-buffer)
+	(deft-mode)))
+    (add-hook 'after-save-hook
+	      #'zettelkasten-mode/after-save-hook)
+    (add-hook 'deft-open-file-hook
+	      #'zettelkasten-mode/deft-open-file-hook))
+   (t
+    (remove-hook 'after-save-hook
+		 #'zettelkasten-mode/after-save-hook))))
+
+(defun zettelkasten-mode/after-save-hook ()
+  (save-excursion
+    (deft-cache-update-file buffer-file-name)
+    (if (deft-buffer-visible-p)
+	(deft-refresh-filter)
+      (setq deft-pending-updates t))))
+
+(defun zettelkasten-mode/deft-open-file-hook ()
+  ;; We don't need to update the cache twice if deft is already doing
+  ;; it. This is a tiny bit hacky but I'm fine with it.
+  (remove-hook 'after-save-hook
+	       #'zettelkasten-mode/after-save-hook))
 
 (add-hook 'org-mode-hook #'org-mode-hook/zettelkasten-mode)
 (defun org-mode-hook/zettelkasten-mode ()
@@ -34,24 +58,23 @@
                          :follow #'org-zettel-follow-link
                          :store #'org-zettel-store-link)
 
-(defun zettel--deft-init-in-background ()
+
+(defun zettel--deft-init ()
   "Initialize deft in the background if it's required before
 ~deft-mode~ has been called."
   (message "Initializing deft...")
-  (with-current-buffer (get-buffer-create "*Deft*")
-    (if (not (eq major-mode 'deft-mode))
-	(let ((inhibit-message t))
-	  (deft-mode))))
+  (require 'deft)
+  (deft-cache-initialize)
+  (deft-cache-update-all)
+  (deft-filter-initialize)
   (message "Done"))
 
 (defun org-zettel-follow-link (id)
   "Follow a zettel: link in org-mode."
-  (unless (get-buffer "*Deft*")
-    (zettel--deft-init-in-background))
-  (let ((files (zettel--filter-string id nil t)))
-    (unless (eq (length files) 1)
-      (user-error "ID Error. Either no or multiple zettelkasten notes found with ID %s" id))
-    (deft-open-file (car files))))
+  (let ((file (zettel--file-from-id id)))
+    (if file
+	(deft-open-file file)
+      (user-error "ID Error. Either no or multiple zettelkasten notes found with ID %s" id))))
 
 (defun org-zettel-store-link ()
   "Store a link to a zettel note."
@@ -63,15 +86,6 @@
        :description (cdr zettel-info)))))
 
 ;; User-facing functions
-
-(defun zettel--new-note (title)
-  "Create a new zettelkasten note."
-  (let* ((zettel-id (format-time-string zettel-id-format))
-	 (filename (concat zettel-id " " title)))
-    (deft-new-file-named filename)
-    (insert (concat "#+TITLE: " title "\n"
-		    "#+TAGS: "))
-    filename))
 
 (defun zettel-new-public-note (title)
   "Create a new private zettelkasten note."
@@ -104,6 +118,15 @@
 
 ;; Internal
 
+(defun zettel--new-note (title)
+  "Create a new zettelkasten note."
+  (let* ((zettel-id (format-time-string zettel-id-format))
+	 (filename (concat zettel-id " " title)))
+    (deft-new-file-named filename)
+    (insert (concat "#+TITLE: " title "\n"
+		    "#+TAGS: "))
+    filename))
+
 (defconst zettel--tag-regexp-format-string "#\\+TAGS:.*? \\(?:#?\\)%s[ \n]"
   "Format string for converting a tag to a regexp that matches
   the tag. Can accept both non-prefixed and #-prefixed tags.")
@@ -113,9 +136,6 @@
 zettelkasten note."
   (format zettel--tag-regexp-format-string (regexp-quote tag)))
 
-;; TODO: Deft assumes that files have been opened with it to hook
-;; saves to update its cache. Either fix the root problem by adding a
-;; hook to zettelkasten-mode or update cache inside zettel--validate.
 (defun zettel--validate ()
   "Validate my zettelkasten to ensure that no public notes
 forward-link to private notes."
@@ -173,6 +193,11 @@ wish to look for forward-links to. Files must be in the deft
 cache for this function to work."
   (zettel--filter `(,(zettel--note-to-link-format note)) files))
 
+(defun zettel--file-from-id (id)
+  (let ((files (zettel--filter-string id nil t)))
+    (when (= (length files) 1)
+      (car files))))
+
 (defconst zettel--all-tags-regexp "#\\+TAGS: \\(?:#?\\)\\(?1:.*\\)\n")
 (defconst zettel--all-tags-separators " \\(?:#?\\)")
 
@@ -197,7 +222,7 @@ note."
       (goto-char (point-min))
       (while (re-search-forward zettel--forward-links-regexp nil t)
 	(push (match-string-no-properties 1) forward-links)))
-    forward-links))
+    (mapcar #'zettel--file-from-id forward-links)))
 
 (defun zettel--get-backlinks ()
   "Gets the backlinks to the current buffer's zettelkasten note."
